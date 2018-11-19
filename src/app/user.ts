746309734bccf3e11bc03f6737e5f375e18a4276
Arianna1995 <arianna.fedeli@studenti.unicam.it>
2018-11-19 17:39:39 +0100

    Form per la compilazione della registrazione creato. Gestioni non ancora gestite

--- src/app/user.ts
+++ src/app/user.ts
@@ -4,14 +4,13 @@ export class User {
       public name: string,
       public surname: string,
       public date: Date, //da vedere se è date o string
-      public email: string,
       public account: Account
     ) {}
-  
   }
 
-  export class Account{
+  export class Account {
       constructor(
+          public email: string,
           public username:string,
           public password:string
       ){}
