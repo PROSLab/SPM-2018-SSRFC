f632fde7085a202f9a03aac5929da5f8c42b9e50
EmanueleSc <scalaemanuele92@gmail.com>
2018-11-20 10:45:06 +0100

    first commit: reactive server architecture and signin API

---
+++ SPMServer/src/test/java/com/spm/api/SpmServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spm.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpmServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
