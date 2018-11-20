ff9e2952cecc711c92896d75b8ccc6d33fb08319
Arianna1995 <arianna.fedeli@studenti.unicam.it>
2018-11-20 13:00:12 +0100

    modifica della struttura dell'oggetto utente

--- src/app/user.ts
+++ src/app/user.ts
@@ -3,15 +3,13 @@ export class User {
     constructor(
       public name: string,
       public surname: string,
-      public date: Date, //da vedere se è date o string
       public account: Account
     ) {}
   }
 
   export class Account {
       constructor(
-          public email: string,
-          public username:string,
+          public email: string, // è l'username
           public password:string
       ){}
   }
