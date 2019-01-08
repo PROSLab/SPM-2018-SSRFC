7231e2b31521ce2178a110d20a8bcb4c56593de9
EmanueleSc <scalaemanuele92@gmail.com>
2019-01-08 15:44:46 +0100

    share repository API done

--- SPMServer/src/main/java/com/spm/api/handlers/ShareHandler.java
+++ SPMServer/src/main/java/com/spm/api/handlers/ShareHandler.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.spm.api.exceptions.BadRequestException;
+import com.spm.api.exceptions.EmailException;
 import com.spm.api.services.ShareService;
 import com.spm.api.utils.Responses;
 
@@ -21,8 +23,14 @@ public class ShareHandler {
 
 
 	public Mono<ServerResponse> shareRepository(ServerRequest request) {
+		String idRepository = request.queryParam("idRepository").get();
+		String emailTo = request.queryParam("emailTo").get();
 		
-		return Responses.ok("OK");
+		return shareService.updateRepoVisibility(idRepository, true)
+				.flatMap(notUsed -> shareService.sendEmailRepositoryLink(idRepository, emailTo))
+				.flatMap(res -> Responses.ok(res))
+				.onErrorResume(EmailException.class, Responses::badRequest)
+				.onErrorResume(BadRequestException.class, Responses::badRequest);
 	}
 
 }
