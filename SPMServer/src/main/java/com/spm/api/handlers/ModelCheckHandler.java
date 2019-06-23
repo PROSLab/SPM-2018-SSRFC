fda1a8e62dcd1be30981349d2bf70737bb830a94
EmanueleSc <scalaemanuele92@gmail.com>
2019-06-23 15:31:28 +0200

    model check api - upload models api for generate aut files

---
+++ SPMServer/src/main/java/com/spm/api/handlers/ModelCheckHandler.java
@@ -0,0 +1,97 @@
+package com.spm.api.handlers;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyExtractors;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.spm.api.utils.Choreography;
+import com.spm.api.utils.Collaboration;
+import com.spm.api.utils.Responses;
+import com.spm.api.utils.InputStreamCollector;
+
+import reactor.core.publisher.Mono;
+
+@SuppressWarnings("deprecation")
+@Component
+public class ModelCheckHandler {
+	
+	private static String collaborationFolder = System.getProperty("java.io.tmpdir");
+	private static String choreographyFolder = System.getProperty("java.io.tmpdir");
+	private Map<String, Part> map;
+	private InputStream collaborationInputStream;
+	private InputStream choreographyInputStream;
+	
+	@SuppressWarnings({ "unchecked" })
+	public Mono<ServerResponse> uploadModel(ServerRequest request) {
+		return request.body(BodyExtractors.toMultipartData())
+				.flatMap(parts -> {
+					map = parts.toSingleValueMap();
+					
+					return map.get("collaboration").content().collect(
+							InputStreamCollector::new, 
+							(t, dataBuffer) -> t.collectInputStream(dataBuffer.asInputStream())
+					);
+					
+				})
+				.flatMap(is -> {
+					collaborationInputStream = is.getInputStream();
+					return Mono.just(true);
+				})
+				.flatMap(notUsed -> {
+					return map.get("choreography").content().collect(
+							InputStreamCollector::new, 
+							(t, dataBuffer) -> t.collectInputStream(dataBuffer.asInputStream())
+					);
+				})
+				.flatMap(is -> {
+					choreographyInputStream = is.getInputStream();
+					return Mono.just(true); 
+				})
+				.flatMap(parts -> {
+					
+					File collaborationFile;
+					File choreographyFile;
+					
+					try {
+						collaborationFile = File.createTempFile("collaboration", ".aut", new File(collaborationFolder));
+						choreographyFile = File.createTempFile("choreography", ".aut", new File(choreographyFolder));
+					} catch (IOException e) {
+						e.printStackTrace();
+						return Mono.error(new Exception(e.getMessage()));
+					}
+					
+					Collaboration coobj = new Collaboration();
+					Choreography chobj = new Choreography();
+					
+					ArrayList<String> choreographyActions =	chobj.init(choreographyInputStream, false, choreographyFile);
+					coobj.setChoreographyActions(choreographyActions);
+					coobj.init(collaborationInputStream, false, collaborationFile);
+					
+					
+					JSONObject obj = new JSONObject();
+					obj.put("choreography", choreographyFile.getName());
+					obj.put("collaboration", collaborationFile.getName());
+					StringWriter out = new StringWriter();
+					try {
+						obj.writeJSONString(out);
+					} catch (IOException e) {
+						e.printStackTrace();
+						return Mono.error(new Exception(e.getMessage()));
+					}
+					String jsonText = out.toString();
+					
+					return Responses.ok(jsonText);
+				})
+				.onErrorResume(Exception.class, Responses::internalServerError);
+	}
+}
---
+++ SPMServer/src/main/java/com/spm/api/routes/ModelCheckRouter.java
@@ -0,0 +1,35 @@
+package com.spm.api.routes;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.spm.api.handlers.ModelCheckHandler;
+
+@Configuration
+public class ModelCheckRouter {
+	
+	@Bean
+	public RouterFunction<ServerResponse> modelCheckRoutes(ModelCheckHandler modelCheckHandler) {
+		return RouterFunctions
+				.nest(
+						path("/api/modelcheck"),
+						RouterFunctions.route(
+								POST("/upload").and(accept(MediaType.MULTIPART_FORM_DATA)),
+								modelCheckHandler::uploadModel
+						)
+						/*.andRoute(
+								GET("/file"),
+								shareHandler::shareModel
+						)*/
+				);
+	}
+
+}
